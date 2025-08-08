70c42d2b3648769d9c534a2624026e7ec88c0e3b
Alan Zhu <alanzhu9000@gmail.com>
2025-08-08 10:16:01 -0400

    server added authroute

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import authRoute from "./routes/authRoute";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
@@ -10,6 +12,9 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// routes
+app.use("/api/auth/", authRoute);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
