17fcb9a0dad1172f1767c1813b0631adcb79ec17
Alan Zhu <alanzhu9000@gmail.com>
2025-08-19 11:30:12 -0400

    removed os import

--- backend/src/socket/socketHandlers.ts
+++ backend/src/socket/socketHandlers.ts
@@ -1,7 +1,6 @@
 import { Server as SocketIOServer, Socket } from "socket.io";
 import { prisma } from "../lib/prisma";
 import { ActiveUser, ChatMessageData } from "../types";
-import { userInfo } from "os";
 
 // store active users, maybe later move to redis
 let activerUsers: ActiveUser[] = [];
